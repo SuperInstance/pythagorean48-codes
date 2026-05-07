21834f1075aff5e0b2edb6ee3e3821d34cb22163
Forgemaster <forgemaster@superinstance.dev>
2026-05-07 12:31:22 -0800

    build artifacts

---
+++ target/debug/deps/pythagorean48_codes-5ea8cd43d570ab80.d
@@ -0,0 +1,5 @@
+/tmp/pythagorean48-codes/target/debug/deps/pythagorean48_codes-5ea8cd43d570ab80.d: src/lib.rs
+
+/tmp/pythagorean48-codes/target/debug/deps/pythagorean48_codes-5ea8cd43d570ab80: src/lib.rs
+
+src/lib.rs:
